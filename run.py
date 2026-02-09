4740999262f58cf23c9de5cfcf61361e61a14cf1
Turja1111 <saumik.das.turja@gmail.com1>
2026-02-09 13:18:12 +0600

    upfate

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
-from flaskblog import app
+from flaskblog import create_app, db
 
-if __name__ == '__main__':
+app = create_app()
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
